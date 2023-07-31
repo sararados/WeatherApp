b9148dbdfdbef74f7d67fe1457e342a0c4c2595f
Sara Rados <sararados@hotmail.com>
2023-08-01 01:07:33 +0200

    fix: change image src

--- public/script.js
+++ public/script.js
@@ -21,15 +21,15 @@ async function fetchApiKey() {
           document.querySelector(".wind").innerHTML = data.wind.speed + " km/h"; 
 
           if (data.weather[0].main == "Clouds") {
-            weatherIcon.src = "images/clouds.png";
+            weatherIcon.src = "public/images/clouds.png";
           } else if (data.weather[0].main == "Clear") {
-            weatherIcon.src = "images/clear.png";
+            weatherIcon.src = "public/images/clear.png";
           } else if (data.weather[0].main == "Rain") {
-            weatherIcon.src = "images/rain.png";
+            weatherIcon.src = "public/images/rain.png";
           } else if (data.weather[0].main == "Drizzle") {
-            weatherIcon.src = "images/drizzle.png";
+            weatherIcon.src = "public/images/drizzle.png";
           } else if (data.weather[0].main == "Mist") {
-            weatherIcon.src = "images/mist.png";
+            weatherIcon.src = "public/images/mist.png";
           }
 
           document.querySelector(".weather").style.display = "block";
