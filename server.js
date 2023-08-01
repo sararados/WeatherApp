ea1d5423831adf52bde1143a2f25b9e8414a517a
Sara Rados <sararados@hotmail.com>
2023-08-01 02:09:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -31,5 +31,5 @@ app.use(express.static('public'));
 // Start the server
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on https://localhost:${port}`);
 });
